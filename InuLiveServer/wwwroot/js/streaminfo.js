dcddf1d047fa0558a597b5ca2bd4c94af65a8356
aiex718 <aiex718g@gmail.com>
2025-03-22 08:32:20 +0800

    Update client for sync time and stream info with media url

--- InuLiveServer/wwwroot/js/streaminfo.js
+++ InuLiveServer/wwwroot/js/streaminfo.js
@@ -5,44 +5,34 @@ const StreamInfoEvents = {
 	'StreamInfoEmpty': 'StreamInfoEmpty'
 };
 
-var StreamInfoUrl = location.protocol + '//' + location.host +"/api/streaminfo" ;
-
+var StreamInfoApiUrl = location.protocol + '//' + location.host +"/api/streaminfo" ;
  
 $(document).ready(function() 
 {
-	//window.setTimeout(function(){
-		var jqxhr = $.ajax({
-			url: StreamInfoUrl, 
-			timeout: 5000,
-			beforeSend: function(){
-				var evt = new CustomEvent(StreamInfoEvents['StreamInfoQuerying'], {'detail': StreamInfoUrl});
-				window.dispatchEvent(evt);
-			},
-			success: function(data) 
-			{	
-				if (data) {
-					var stream_title = $('#stream_title')[0];
-					var stream_subtitle = $('#stream_subtitle')[0];
-					var stream_game = $('#stream_game')[0];
-
-					stream_title.innerHTML = data.title;
-					stream_subtitle.innerHTML = data.subtitle;
-					stream_game.innerHTML = data.game;
-
-					var evt = new CustomEvent(StreamInfoEvents['StreamInfoReady'], { 'detail': data });
-					window.dispatchEvent(evt);
-				}
-				else
-				{
-					var evt = new CustomEvent(StreamInfoEvents['StreamInfoEmpty'], { });
-					window.dispatchEvent(evt);
-				}
-			},
-			error: function(xhr, status, error) 
+	var jqxhr = $.ajax({
+		url: StreamInfoApiUrl, 
+		timeout: 10000,
+		beforeSend: function(){
+			var evt = new CustomEvent(StreamInfoEvents['StreamInfoQuerying'], {'detail': StreamInfoApiUrl});
+			window.dispatchEvent(evt);
+		},
+		success: function(data) 
+		{	
+			if (data) 
 			{
-				var evt = new CustomEvent(StreamInfoEvents['StreamInfoFail'], {'detail': error});
+				var evt = new CustomEvent(StreamInfoEvents['StreamInfoReady'], { 'detail': data });
 				window.dispatchEvent(evt);
 			}
-		});
-	//},500);
+			else
+			{
+				var evt = new CustomEvent(StreamInfoEvents['StreamInfoEmpty'], { });
+				window.dispatchEvent(evt);
+			}
+		},
+		error: function(xhr, status, error) 
+		{
+			var evt = new CustomEvent(StreamInfoEvents['StreamInfoFail'], {'detail': error});
+			window.dispatchEvent(evt);
+		}
+	});
 });
